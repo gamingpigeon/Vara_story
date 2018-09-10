49ea18271226b763c127bcec7542ed46355e0b43
julianvara <julianvar.11917@redlandsschools.net>
2018-09-10 15:25:50 +0000

    need to fix age.py

---
+++ age.py
@@ -0,0 +1,9 @@
+#WRITE A PROGRAM THAT WILL ASK THE USER WHAT THEIR AGE IS AND THEN DETERMINE IF THEY ARE OLD ENOUGH TO VOTE AND RESPOND APPROPIATELY
+
+ask=int(input("how old are you?:"))
+
+if ask =(18):
+    
+    print("2024 for kanye")
+    
+    
--- first.py
+++ first.py
@@ -1,5 +1,5 @@
 #write a program that will add six to the users input and return the results!
-x = int(input("soild number pls:"))
+x = int(input("soild number pls: "))
 
-print(6+x)
+print(str(6+x)+" what did i add to your number")
 
