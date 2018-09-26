64d412864ea7f0d4d6293a9c1faeb9e99aca48e3
julianvara <julianvar.11917@redlandsschools.net>
2018-09-26 15:34:36 +0000

    add the function

---
+++ function.py
@@ -0,0 +1,24 @@
+"""this is all about functions"""
+
+def greeting():
+    print("hola")  
+
+def Askname():
+    name=input("what is your name: ")
+    return name
+def askage():
+    age=int(input("what is your age: "))
+    return age
+def canvote(age):
+    if (age >=18):
+        print("you can vote:) ")
+    else:
+        print("you cannot vote :(")
+    return canvote
+#call the function
+greeting()
+x = Askname() #Save the output of Askname() to variable
+print("hola " + x)
+y = askage()
+z = canvote(y)# z = if they can vote or note (T/F)
+print(z)
